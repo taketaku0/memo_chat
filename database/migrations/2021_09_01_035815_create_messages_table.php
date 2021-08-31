b76c729b066cc04e2934bac7b342a4afbd0891a4
taketaku0 <itotake.taku@gmail.com>
2021-09-01 04:37:00 +0900

    グループ・メッセージ用のマイグレーションとモデルを追加

---
+++ database/migrations/2021_09_01_035815_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('group_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
